1dd51b3d48d54989e937a49eccc7b35cfb044e7a
DaisyLBell <daisybel@usc.edu>
2021-04-29 12:05:28 -0700

    the project

---
+++ final.js
@@ -0,0 +1,12 @@
+const soundBtn = document.querySelector('.play-audio');
+let myAudio = document.querySelector('#audio');
+soundBtn.addEventListener('click', () => {
+    myAudio.play();
+});
+
+
+const soundBtn2 = document.querySelector('.play-audio-2');
+let myAudio2 = document.querySelector('#audio2');
+soundBtn.addEventListener('click', () => {
+    myAudio.play();
+});
